4599e6b2fffa4a84f2d4f49066922ba3fc44b95e
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-06 09:45:04 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_context_menu_relations do
+  name 'Redmine Context Menu Relations plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
